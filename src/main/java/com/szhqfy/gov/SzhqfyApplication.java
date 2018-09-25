da2acc7726173bc9cfc02fc80bdc54016d6adf4e
zhangtejun <1053946416@qq.com>
2018-09-25 19:48:46 +0800

    version1.0

---
+++ src/main/java/com/szhqfy/gov/SzhqfyApplication.java
@@ -0,0 +1,12 @@
+package com.szhqfy.gov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzhqfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzhqfyApplication.class, args);
+	}
+}
